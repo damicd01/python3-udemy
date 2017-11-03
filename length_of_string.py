f8bf240d0cb24d8a47e70691eb1738b4c4b496dd
Dan D'Amico <danny.damico@gmail.com>
2017-11-03 11:49:29 +0000

    added conditional to length of string

--- length_of_string.py
+++ length_of_string.py
@@ -1,7 +1,11 @@
-to_be_calculated = input("Please enter your string = ")
+to_be_calculated = "hello"
+
 
 def calc_length(str_to_be_calc):
     length = len(str_to_be_calc)
     return  length
 
-print (calc_length(to_be_calculated))
+if type(to_be_calculated) == str:
+    print ("The length of the string is " + str(calc_length(to_be_calculated)) + " characters")
+else:
+    print ("You can't calculate the length of this data object")
