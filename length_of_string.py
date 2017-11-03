f6155ba35c0451a3f2937f387b847751cdefd928
Dan D'Amico <danny.damico@gmail.com>
2017-11-03 11:29:46 +0000

    using functions within conditional

---
+++ length_of_string.py
@@ -0,0 +1,7 @@
+to_be_calculated = input("Please enter your string = ")
+
+def calc_length(str_to_be_calc):
+    length = len(str_to_be_calc)
+    return  length
+
+print (calc_length(to_be_calculated))
