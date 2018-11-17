a240c2bd2c0e08e08ba4b013a20ec1e654ef8f5f
Dan D'Amico <danny.damico@gmail.com>
2018-11-17 05:42:25 +0000

    added a new stableford calc

--- stableford/mystableford.py
+++ stableford/mystableford.py
@@ -1,95 +1,42 @@
-def stableford_points(hcp,par,index,score):
-    sf_score = { -4:6, -3:5, -2:4, -1:3, 0:2, 1:1, 2:0 }
-    diff = score - par
-    if hcp > 18 and index <= hcp % 18 and diff == -4:
-        return ((sf_score.get(diff)) + 2)
-    elif hcp > 18 and index >= hcp % 18 and diff == -4:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp > 18 and index <= hcp % 18 and diff == -3:
-        return ((sf_score.get(diff)) + 2)
-    elif hcp > 18 and index >= hcp % 18 and diff == -3:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp > 18 and index <= hcp % 18 and diff == -2:
-        return ((sf_score.get(diff)) + 2)
-    elif hcp > 18 and index >= hcp % 18 and diff == -2:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp > 18 and index <= hcp % 18 and diff == -1:
-        return ((sf_score.get(diff)) + 2)
-    elif hcp > 18 and index >= hcp % 18 and diff == -1:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp > 18 and index <= hcp % 18 and diff == 0:
-        return ((sf_score.get(diff)) + 2)
-    elif hcp > 18 and index >= hcp % 18 and diff == 0:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp > 18 and index <= hcp % 18 and diff == 1:
-        return ((sf_score.get(diff)) + 2)
-    elif hcp > 18 and index >= hcp % 18 and diff == 1:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp > 18 and index <= hcp % 18 and diff == 2:
-        return ((sf_score.get(diff)) + 2)
-    elif hcp > 18 and index >= hcp % 18 and diff == 2:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp == 18 and diff == -4:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp == 18 and diff == -3:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp == 18 and diff == -2:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp == 18 and diff == -1:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp == 18 and diff == 0:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp == 18 and diff == 1:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp == 18 and diff == 2:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp < 18 and index <= hcp % 18 and diff == -4:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp < 18 and index > hcp % 18 and diff == -4:
-        return ((sf_score.get(diff)))
-    elif hcp < 18 and index <= hcp % 18 and diff == -3:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp < 18 and index > hcp % 18 and diff == -3:
-        return ((sf_score.get(diff)))
-    elif hcp < 18 and index <= hcp % 18 and diff == -2:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp < 18 and index > hcp % 18 and diff == -2:
-        return ((sf_score.get(diff)))
-    elif hcp < 18 and index <= hcp % 18 and diff == -1:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp < 18 and index > hcp % 18 and diff == -1:
-        return ((sf_score.get(diff)))
-    elif hcp < 18 and index <= hcp % 18 and diff == 0:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp < 18 and index > hcp % 18 and diff == 0:
-        return ((sf_score.get(diff)))
-    elif hcp < 18 and index <= hcp % 18 and diff == 1:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp < 18 and index > hcp % 18 and diff == 1:
-        return ((sf_score.get(diff)))
-    elif hcp < 18 and index <= hcp % 18 and diff == 2:
-        return ((sf_score.get(diff)) + 1)
-    elif hcp < 18 and index > hcp % 18 and diff == 2:
-        return ((sf_score.get(diff)))
+def shots_per_hole(hcp,index):
+    if hcp > 18 and hcp % 18 >= index:
+        return 2
+    elif hcp > 18 and hcp % 18 < index:
+        return 1
+    elif hcp == 18:
+        return 1
+    elif hcp < 18 and hcp % 18 <= index:
+        return 1
     else:
-        return '0'
+        return 0
 
-stableford_score = 0
-starting_hole = 1
+
+sf_score = { -4:6, -3:5, -2:4, -1:3, 0:2, 1:1, 2:0 }
+
+def stableford_score(hcp,par,score,index):
+    if hcp > 18 and hcp % 18 >= index:
+        par = par + shots_per_hole(hcp,index)
+        return(sf_score.get(score-par))
+    elif hcp > 18 and hcp % 18 < index:
+        par = par + shots_per_hole(hcp,index)
+        return(sf_score.get(score-par))
+    elif hcp == 18:
+        par = par + shots_per_hole(hcp,index)
+        return(sf_score.get(score-par))
+    elif hcp < 18 and hcp % 18 >= index:
+        par = par + shots_per_hole(hcp,index)
+        return(sf_score.get(score-par))
+
+stableford_points = 0
+
 hcp = int(input("What is your hcp = "))
-course = input("Where did you play? ")
 
-myfile = open("scorecard.txt", "w")
-myfile.write(course)
 
 for hole in range(0,2):
     par = int(input("Hole par = "))
-    myfile.write(str(par))
     score = int(input("Hole score = "))
-    myfile.write(str(score))
     index = int(input("Hole stroke index = "))
-    myfile.write(str(index))
-    stableford_score += stableford_points(hcp,par,index,score)
+    stableford_points += stableford_score(hcp,par,score,index)
 
-myfile.write(str(stableford_score))
-print("Your stableford score is", stableford_score)
+
+print("Your stableford score is", stableford_points)
